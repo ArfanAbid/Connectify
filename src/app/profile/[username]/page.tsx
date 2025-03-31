d48818a12ad94b0cbd97382c8d68336a45b3582c
Arfan Abid <iffionex111@gmail.com>
2025-03-31 23:32:48 +0500

    Profile section done

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
-async function ProfilePage({params}:{params:{username:string}}) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));// wait three second and then render the loading part
-    return (
-    <div>ProfilePage</div>
-  )
+export async function generateMetadata({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
 }
 
-export default ProfilePage
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
